2f8abff65d907c86441085344801b6501df0617e
Nemanja Sreckovic <nemanja.sreckovic@gmail.com>
2018-04-18 18:46:23 +0200

    recipeView

--- src/js/models/Recipe.js
+++ src/js/models/Recipe.js
@@ -15,7 +15,7 @@ export default class Recipe {
       );
       this.title = res.data.recipe.title;
       this.author = res.data.recipe.publisher;
-      this.img = res.data.recipe.img_url;
+      this.img = res.data.recipe.image_url;
       this.url = res.data.recipe.source_url;
       this.ingredients = res.data.recipe.ingredients;
     } catch (err) {
