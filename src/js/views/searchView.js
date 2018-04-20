af05b5ad246e712f9183bd0a7a71773eb2f40662
Nemanja Sreckovic <nemanja.sreckovic@gmail.com>
2018-04-20 14:01:49 +0200

    highlightSelected

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -12,9 +12,13 @@ export const clearResults = () => {
 };
 
 export const highlightSelected = id => {
+  const resultsArr = Array.from(document.querySelectorAll('.results__link'));
+  resultsArr.forEach(el => {
+    el.classList.remove('results__link--active');
+  });
   document
-    .querySelector(`a[href=#${id}]`)
-    .classList.add('.results__link--active');
+    .querySelector(`a[href="#${id}"]`)
+    .classList.add('results__link--active');
 };
 
 const renderRecipe = recipe => {
