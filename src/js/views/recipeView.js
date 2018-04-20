7042ae49a49eac69fbf94d95545dd16650165ee1
Nemanja Sreckovic <nemanja.sreckovic@gmail.com>
2018-04-20 13:13:16 +0200

    formatCount

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,7 +1,31 @@
 import { elements } from './base';
+import { Fraction } from 'fractional';
 
 export const clearRecipe = () => {
   elements.recipe.innerHTML = '';
+};
+
+const formatCount = count => {
+  if (count) {
+    // 2.5 -> 5/2 -> 2 1/2
+    // 0.5 -> 1/2
+    const [int, dec] = count
+      .toString()
+      .split('.')
+      .map(el => parseInt(el, 10));
+
+    if (!dec) return count;
+
+    if (int === 0) {
+      const fr = new Fraction(count);
+      return `${fr.numerator}/${fr.denominator}`;
+    } else {
+      const fr = new Fraction(count - int);
+      return `${int} ${fr.numerator}/${fr.denominator}`;
+    }
+  }
+
+  return '?';
 };
 
 const createIngredient = ing => `
@@ -9,7 +33,7 @@ const createIngredient = ing => `
     <svg class="recipe__icon">
       <use href="img/icons.svg#icon-check"></use>
     </svg>
-    <div class="recipe__count">${ing.count}</div>
+    <div class="recipe__count">${formatCount(ing.count)}</div>
     <div class="recipe__ingredient">
       <span class="recipe__unit">${ing.unit}</span>
       ${ing.ingredient}
